6dac734d05f4ba1027b7410f12d20bda2fc4b08e
Alan Couch <alan@couch.net.au>
2014-09-15 13:32:09 +1000

    Added require for coorgram package up front..

--- lib/FeelForData.R
+++ lib/FeelForData.R
@@ -6,11 +6,12 @@ FeelForData <- function(df) {
   #
   # Returns:
   #   The data with a variety of summaries.
+  require(corrgram)
   df <- AllOtoData
   # Error handling
   str(df)
   corrgram(df)
-  #sapply(df[1,],class)
+  sapply(df[1,],class)
   unique(df$LarvalID)
   #table(df$Time,df$Ba)
 }
